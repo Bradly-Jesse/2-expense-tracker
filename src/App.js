c62f7c307ba0bb534e1bbbab0ee24ed2b5e03aa3
Jesse <eoshout@Gmail.com>
2022-03-13 18:25:47 -0400

    Finished ExpenseTracker

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import Header from './Components/Atom/Header';
-import Balance from './Components/Atom/Balance'
+import Header from "./Components/Atom/Header";
+import Balance from "./Components/Atom/Balance";
+import IncomeExpenses from "./Components/Atom/IncomeExpenses";
+import TransactionList from "./Components/Molecule/TransactionList";
+import AddTransaction from "./Components/Organisms/AddTransaction";
+
+import {GlobalProvider} from "./context/GlobalState";
 function App() {
+
   return (
-    <div>
-      <Header title={"Expense Tracker"}/>
+    <GlobalProvider>
+      <Header title={"Expense Tracker"} />
       <div className="container">
-        <Balance/>
-        </div>
-    </div>
+        <Balance />
+        <IncomeExpenses />
+        <TransactionList />
+        <AddTransaction />
+      </div>
+    </GlobalProvider>
   );
 }
 
